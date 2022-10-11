5ad39d7f9de16cd3b1210b3079e2356790aa55d5
AIMMY-CAMTPC\noppo <noppon.w@cmu.ac.th>
2022-10-11 09:57:45 +0700

    Change update from 2022 Template

--- MonoGame-Sample/Game1.cs
+++ MonoGame-Sample/Game1.cs
@@ -2,31 +2,18 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 
-using MonoGame.Extended.Tiled;
-using MonoGame.Extended.Tiled.Renderers;
-
-namespace MonoGame_Sample
+namespace Monogame_Sample
 {
     public class Game1 : Game
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        Texture2D myTexture;
-        Vector2 myBGPosition;
-
-        TiledMap _tiledMap;
-        TiledMapRenderer _tiledMapRenderer;
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
-
-            _graphics.PreferredBackBufferWidth = 960;
-            _graphics.PreferredBackBufferHeight = 640;
-
-            myBGPosition = Vector2.Zero;
         }
 
         protected override void Initialize()
@@ -34,16 +21,10 @@ namespace MonoGame_Sample
             // TODO: Add your initialization logic here
 
             base.Initialize();
-            myTexture = Content.Load<Texture2D>("bgCloud");
-
         }
 
         protected override void LoadContent()
         {
-            _tiledMap = Content.Load<TiledMap>("Level");
-            _tiledMapRenderer = new TiledMapRenderer(GraphicsDevice, _tiledMap);
-
-
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             // TODO: use this.Content to load your game content here
@@ -55,7 +36,7 @@ namespace MonoGame_Sample
                 Exit();
 
             // TODO: Add your update logic here
-            _tiledMapRenderer.Update(gameTime);
+
             base.Update(gameTime);
         }
 
@@ -64,10 +45,7 @@ namespace MonoGame_Sample
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             // TODO: Add your drawing code here
-            _spriteBatch.Begin();
-            _spriteBatch.Draw(myTexture, myBGPosition, Color.White);
-            _tiledMapRenderer.Draw();
-            _spriteBatch.End();
+
             base.Draw(gameTime);
         }
     }
